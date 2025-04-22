dbe147d2ff36540f0a6e31807ce00a078419c426
OliveiraL21 <lucasoliveira508@gmail.com>
2025-04-22 11:58:46 -0300

    finalizando menu

--- reforma-reparo-fogoes-react/src/components/navbar/Navbar.tsx
+++ reforma-reparo-fogoes-react/src/components/navbar/Navbar.tsx
@@ -1,5 +1,9 @@
 import './Nav.css';
 
+const sendToWhatsApp = () => {
+	window.open('https://wa.me/5519996521626/?text=ol%c3%a1+!+gostaria+de+solicitar+um+diagnostico+do+meu+fog%c3%a3o', '_blank');
+}
+
 function Navbar() {
 	return (
 		<nav className='navbar__container'>
@@ -8,10 +12,14 @@ function Navbar() {
 				<span className="company__name">Marcos Reformas e Reparo de Fogões</span>
 			</div>
 			<ul className='navbar__menulist'>
-				<li className='navbar__menulist-item'>Sobre Nós</li>
-				<li className='navbar__menulist-item'>Como nos achar</li>
-				<li>
-					<button >Contato</button>
+				<li className='navbar__menulist-item'>
+					<a className='navbar__menulist-item-text' href="#sobreNos">Sobre Nós</a>
+				</li>
+				<li className='navbar__menulist-item'>
+					<a href="#" className='navbar__menulist-item-text'>Como nos achar</a>
+				</li>
+				<li className='navbar__menulist-item'>
+					<button className='navbar__menu__button' onClick={sendToWhatsApp}>Contato</button>
 				</li>
 			</ul>
 		</nav>
