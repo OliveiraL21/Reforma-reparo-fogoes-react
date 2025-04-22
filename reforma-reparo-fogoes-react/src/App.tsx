5479b696367d81bf3831174dccf5e939e93cef09
OliveiraL21 <lucasoliveira508@gmail.com>
2025-04-22 12:58:11 -0300

    implementando conteudo main

--- reforma-reparo-fogoes-react/src/App.tsx
+++ reforma-reparo-fogoes-react/src/App.tsx
@@ -12,7 +12,14 @@ function App() {
         <Navbar></Navbar>
       </header>
       <main>
-
+        <section className='maincontent__container'>
+          <div className='maincontent__sloganSection'>
+            <h1 className='maincontent__sloganSection-title'>Marcos Reformas de fogões</h1>
+            <p className='maincontent__sloganSection'>O Especialista em reforma e reparos de fogões que você precisa, para nunca ficar na mão !</p>
+            <button>Agendar Reparo</button>
+          </div>
+          <div></div>
+        </section>
       </main>
       <footer>
 
