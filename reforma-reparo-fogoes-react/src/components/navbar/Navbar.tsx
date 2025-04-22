fd9740ee0986588264ea10333ce5d69f84eb2971
Lucas J D Oliveira <ljoliveira99@outlook.com>
2025-04-21 23:10:29 -0300

    ajustes

--- reforma-reparo-fogoes-react/src/components/navbar/Navbar.tsx
+++ reforma-reparo-fogoes-react/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
 	return (
 		<nav className='navbar__container'>
 			<div className='navbar__logo-container'>
-				<img height={30} width={30} src='../../assets/images/cropped-tools__favicon.png' alt="logotipo da empresa" />
+				<img height={30} width={30} src='/assets/images/cropped-tools__favicon.png' alt="logotipo da empresa" />
 				<span className="company__name">Marcos Reformas e Reparo de Fogões</span>
 			</div>
 			<ul className='navbar__menulist'>
